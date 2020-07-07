8793564225a2be97c4e45e3c2afbe5667f962ee8
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-07-07 04:10:10 +0000

    short url added

--- express_server.js
+++ express_server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
 app.set("view engine", "ejs");
-
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,21 +12,24 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-//added #1
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-//
 
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[req.params.shortURL];
+  let templateVars = { shortURL: shortURL, longURL: longURL };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
