ba0f0bc08f6c2e746e6af5efe84ec13b7c2a87b8
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-07-07 03:07:32 +0000

    ejs templates filled

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -12,9 +13,19 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+//added #1
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+//
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
